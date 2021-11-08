ad23910385192fdb7649a6a85bacd986dd40fa86
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-11-08 20:38:02 +0100

    Ehn: Styled Informations pratiques page

--- src/modules/views/InformationsPratiques.js
+++ src/modules/views/InformationsPratiques.js
@@ -1,91 +1,45 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import Button from '../components/Button';
-import Typography from '@mui/material/Typography';
-
-const item = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  px: 5,
-};
-
-const number = {
-  fontSize: 24,
-  fontFamily: 'default',
-  color: 'secondary.main',
-  fontWeight: 'medium',
-};
-
-const image = {
-  height: 55,
-  my: 4,
-};
+import {HeaderTitlePurple, SubtitlePurple, TextPurple, useStyles} from "../components/Styles";
 
 function InformationsPratiques() {
+    const classes = useStyles();
   return (
-    // <Box
-    //   component="section"
-    //   sx={{ display: 'flex', bgcolor: 'secondary.light', overflow: 'hidden' }}
-    // >
-      // <Container
-      //   sx={{
-      //     mt: 10,
-      //     mb: 15,
-      //     position: 'relative',
-      //     display: 'flex',
-      //     flexDirection: 'column',
-      //     alignItems: 'center',
-      //   }}
-      // >
-      //   <Box
-      //     component="img"
-      //     src="/static/themes/onepirate/productCurvyLines.png"
-      //     alt="curvy lines"
-      //     sx={{
-      //       pointerEvents: 'none',
-      //       position: 'absolute',
-      //       top: -180,
-      //       opacity: 0.7,
-      //     }}
-      //   />
-      <Container>
-        <Typography variant="h1" marked="center" component="h2" sx={{ mb: 14 }}>
-          Informations Pratiques        
-        </Typography>
-        <p>
-      Être habillé confortablement (training, habits souples par exemple), vous allez gar- der vos vêtements.
-      </p>
-      <h2>
-      Tarifs et durée
-      </h2>
-      <p>
-      Une séance coûte CHF 100.–
-      Paiement en espèce uniquement (non remboursé par les caisses maladie).
-      Durée approximative : 60 à 90 minutes (première séance), puis entre 45 et
-      60 minutes pour les suivantes. A noter que le nombre de séance ne peut être défini en avance, la prise en charge est en fonction de chaque individu.
-      </p>
-      <h2>
-      Horaires & lieu
-      </h2>
-      <p>
-      Tous les mercredis après-midi, au cabinet ostéopathie XXXXXXX, Notre Dame de la route XX, Villars-sur-Glâne
-      </p>
-      <h2>
-      Liens externes      
-      </h2>
-      <p>
-      Site de Benoît Tremblay : www.pneumacorps.com
-      Site de Union PneumaCorps France : pneumacorps2021.wixsite.com/my-site-1     
-      </p>
-      <h2>
-      Informations Covid
-      </h2>
-      <p>ˆ
-      Les mesures usuelles sont en vigueur.   
-      </p>
+      <Container className={classes.purpleLightBox}>
+          <HeaderTitlePurple>
+              Informations Pratiques
+          </HeaderTitlePurple>
+          <TextPurple>
+                Être habillé confortablement (training, habits souples par exemple), vous allez gar- der vos vêtements.
+          </TextPurple>
+          <SubtitlePurple>
+          Tarifs et durée
+          </SubtitlePurple>
+          <TextPurple>
+          Une séance coûte CHF 100.–
+          Paiement en espèce uniquement (non remboursé par les caisses maladie).
+          Durée approximative : 60 à 90 minutes (première séance), puis entre 45 et
+          60 minutes pour les suivantes. A noter que le nombre de séance ne peut être défini en avance, la prise en charge est en fonction de chaque individu.
+          </TextPurple>
+          <SubtitlePurple>
+          Horaires & lieu
+          </SubtitlePurple>
+          <TextPurple>
+          Tous les mercredis après-midi, au cabinet ostéopathie XXXXXXX, Notre Dame de la route XX, Villars-sur-Glâne
+          </TextPurple>
+          <SubtitlePurple>
+          Liens externes
+          </SubtitlePurple>
+          <TextPurple>
+          Site de Benoît Tremblay : www.pneumacorps.com
+          Site de Union PneumaCorps France : pneumacorps2021.wixsite.com/my-site-1
+          </TextPurple>
+          <SubtitlePurple>
+          Informations Covid
+          </SubtitlePurple>
+          <TextPurple>ˆ
+          Les mesures usuelles sont en vigueur.
+          </TextPurple>
       </Container>
         
 
