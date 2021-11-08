c81d2bacb5708e773497ce36ce38311117783cc1
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-11-08 19:37:31 +0100

    Ehn: Styled Pneumacorps page

--- src/modules/views/Pneumacorps.js
+++ src/modules/views/Pneumacorps.js
@@ -3,32 +3,78 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import {styled} from "@mui/styles";
+import {makeStyles} from "@mui/styles";
 
 const color = {
     color: ''
 };
 
-const HeaderTitle = styled(Typography)({
+const HeaderTitlePurple = styled(Typography)({
     color: '#914e72',
     padding: '25px',
+    fontSize: '75px',
+    fontFamily: 'dahlia-bold',
+    alignText: 'center',
 });
 
+const HeaderTitleWhite = styled(Typography)({
+    color: '#ffffff',
+    padding: '25px',
+    fontSize: '50px',
+    fontFamily: 'dahlia-bold',
+    alignText: 'center',
+});
+
+const SubtitlePurple = styled(Typography)({
+    color: '#914e72',
+    padding: '25px',
+    fontSize: '27px',
+    fontFamily: 'dahlia-bold',
+});
+
+const SubtitleWhite = styled(Typography)({
+    color: '#ffffff',
+    padding: '25px',
+    fontSize: '75px',
+    fontFamily: 'dahlia-bold',
+});
+
+const TextWhite = styled(Typography)({
+    color: '#ffffff',
+    padding: '25px',
+    fontSize: '16px',
+    fontFamily: 'apercu-light',
+});
+
+const TextPurple = styled(Typography)({
+    color: '#914e72',
+    paddingBottom: '25px',
+    fontSize: '16px',
+    fontFamily: 'apercu-light',
+});
+
+const useStyles = makeStyles({
+    purpleBox: {
+        backgroundColor: '#F4EDF0'
+    },
+});
+
+
 function Pneumacorps() {
+    const classes = useStyles();
     return (
         <div>
-            <Container sx={{textAlign: 'center'}}>
-                <HeaderTitle align="center" variant="h1Pink" marked="center">
+            <Container sx={{textAlign: 'center'}} className={classes.purpleBox}>
+                <HeaderTitlePurple>
                     PneumaCorps
-                </HeaderTitle>
-            </Container>
-            <Container>
-                <Typography color='#914e72' align="center" variant="body" marked="center"><br/>
+                </HeaderTitlePurple>
+                <TextPurple>
                     La pratique que je propose se base sur une approche qui se nomme PneumaCorps.
-                </Typography>
-                <Typography color="inherit" align="center" variant="h2" marked="center">
+                </TextPurple>
+                 <SubtitlePurple>
                     Origine de la pratique
-                </Typography>
-                <p>
+                 </SubtitlePurple>
+                <TextPurple>
                     Cette approche a été développée il y a une quarantaine d’années par Benoît Tremblay,
                     inhalothérapeute québécois. Elle intègre diverses techniques de relaxa- tion, dont le but premier et
                     fondamental est de diminuer les effets du stress chro- nique et d’éviter la limitation de la
@@ -36,11 +82,11 @@ function Pneumacorps() {
                     (RRRI). La vision holistique de l’humain est au cœur de ce travail et tend vers un alignement
                     corps-âme-esprit. Les outils utilisés sont régulièrement actualisés et modifiés par Benoît afin de
                     cor- respondre au mieux aux besoins de chacun.
-                </p>
-                <Typography color="inherit" align="center" variant="h2" marked="center">
+                </TextPurple>
+                <SubtitlePurple>
                     Déroulé d’une séance
-                </Typography>
-                <p>
+                </SubtitlePurple>
+                <TextPurple>
                     Il s’agit d’un travail en profondeur sur des tensions chroniques pour libérer le souffle. La
                     pratique s’adresse à toute personne intéressée par ce propos. Au cours d’une séance, je propose dans
                     un premier temps de prendre conscience des tensions auxquelles nous sommes habitués et que nous
@@ -49,16 +95,16 @@ function Pneumacorps() {
                     l’observation, commence le travail de rééquilibration, qui consiste en des mobilisations, des
                     postures, des étirements doux. Et finalement, la personne aura un ou plusieurs exercices à refaire
                     chez elle, pour ancrer le travail et s’autonomiser au quotidien.
-                </p>
-                <Typography color="inherit" align="center" variant="h2" marked="center">
+                </TextPurple>
+                <SubtitlePurple>
                     A qui s’adresse cette pratique
-                </Typography>
-                <p>
+                </SubtitlePurple>
+                <TextPurple>
                     A toute personne désireuse de respirer plus librement. La respiration étant au cœur de la Vie, cela
                     concerne tout un chacun. Autant une personne qui se consi- dère en bonne que des personnes qui
                     souffres de troubles au quotidien liés au stress (anxiété, troubles du sommeil, fatigue chronique,
                     nervosité, irritabilité, dou- leurs chroniques, problèmes de concentration).
-                </p>
+                </TextPurple>
             </Container>
         </div>
     )
