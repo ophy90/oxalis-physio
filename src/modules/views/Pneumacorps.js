b10e45530e24221351cee625aeaa77ac300f6a52
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-11-17 19:20:39 +0100

    Ehn: Changed pneumacorps 'à qui s'adressent les séances' text

--- src/modules/views/Pneumacorps.js
+++ src/modules/views/Pneumacorps.js
@@ -60,14 +60,13 @@ function Pneumacorps() {
                          Déroulé d’une séance
                      </SubtitlePurple>
                      <TextPurple>
-                         Il s’agit d’un travail en profondeur sur des tensions chroniques pour libérer le souffle. La
-                         pratique s’adresse à toute personne intéressée par ce propos. Au cours d’une séance, je propose dans
-                         un premier temps de prendre conscience des tensions auxquelles nous sommes habitués et que nous
-                         avons finalement « nor- malisées ». Commencer avec ce qui est là dans l’instant, l’évaluation se
-                         fait à la marche, en position assise et couchée. Ensuite, avec les éléments mis en lumière par
-                         l’observation, commence le travail de rééquilibration, qui consiste en des mobilisations, des
-                         postures, des étirements doux. Et finalement, la personne aura un ou plusieurs exercices à refaire
-                         chez elle, pour ancrer le travail et s’autonomiser au quotidien.
+                         Une séance comprend une évaluation de la posture et de la respiration debout, à la marche,
+                         assis et couché. L’observation est basée sur des schémas précis de bandes anatomiques de
+                         stress. Puis le travail se fait à partir de la zone du corps la plus tendue, alternant entre
+                         la position couchée ainsi que la marche comme temps d’intégration. J’utilise principalement
+                         des techniques de mobilisation, des postures, de l’auto-suggestion ou des étirements doux.
+                         En fin de séance, un exercice à faire soi-même peut être suggéré pour ancrer le travail et
+                         s’autonomiser au quotidien.
                      </TextPurple>
                  </div>
                 <div className={classes.blocks}>
@@ -75,10 +74,15 @@ function Pneumacorps() {
                         A qui s’adresse cette pratique
                     </SubtitlePurple>
                     <TextPurple>
-                        A toute personne désireuse de respirer plus librement. La respiration étant au cœur de la Vie, cela
-                        concerne tout un chacun. Autant une personne qui se consi- dère en bonne que des personnes qui
-                        souffres de troubles au quotidien liés au stress (anxiété, troubles du sommeil, fatigue chronique,
-                        nervosité, irritabilité, dou- leurs chroniques, problèmes de concentration).
+                        La respiration étant au cœur de la Vie, cette pratique s’adresse à « tout le monde ». Elle
+                        prend tout son sens pour des personnes qui remarquent qu’elles forcent trop ou dépensent trop
+                        d’énergie au quotidien et souhaitent adopter de nouvelles habitudes.
+                        Elle peut avoir des bienfaits sur des troubles avérés (anxiété, troubles du sommeil, fatigue
+                        chronique, nervosité, irritabilité, douleurs chroniques, problèmes de concentration, troubles
+                        musculo-squelettiques) mais elle ne vise pas à les traiter spécifiquement. En revanche, les
+                        séances PneumaCorps sont complémentaires aux autres techniques ou thérapies (telles que yoga,
+                        ostéopathie, physiothérapie, chant, etc).
+
                     </TextPurple>
                 </div>
             </div>
