9d29ce666d8ff8adf47b2355dbd7214ec3ade000
“Ophélie <ophelie.bugnon@edu.hefr.ch>
2023-02-19 18:36:42 +0100

    Added Instagram and Facebook link in contact page

--- src/modules/views/AppFooter.js
+++ src/modules/views/AppFooter.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {HeaderTitleWhite, TextWhite, useStyles} from "../components/Styles";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 export default function AppFooter() {
     const classes = useStyles();
@@ -18,6 +20,14 @@ export default function AppFooter() {
           <TextWhite>
           info@oxalis-respiration.ch
           </TextWhite>
+          <div>
+              <a href="https://www.instagram.com/oxalis.respiration/" target="_blank" rel="noopener noreferrer">
+                  <InstagramIcon className={classes.contactIcons}/>
+              </a>
+              <a href="https://www.facebook.com/profile.php?id=100078833613396" target="_blank" rel="noopener noreferrer">
+                  <FacebookIcon className={classes.contactIcons}/>
+              </a>
+          </div>
           <TextWhite className={classes.formatCopyRight}>
           © OXALIS 2021. TOUS DROITS RÉSERVÉS.
           </TextWhite>
