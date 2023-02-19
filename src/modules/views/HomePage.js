6f0de0ae958e5876a22657ce6a526ef0ffefffdb
“Ophélie <ophelie.bugnon@edu.hefr.ch>
2023-02-19 18:39:23 +0100

    correcting typo

--- src/modules/views/HomePage.js
+++ src/modules/views/HomePage.js
@@ -20,7 +20,7 @@ export default function HomePage() {
       <img
         style={{ display: 'none' }}
         src={backgroundImage}
-        alt="increase priority"
+        alt={"increase priority"}
       />
         <HeaderTitleWhiteHomePage>
           posture et
