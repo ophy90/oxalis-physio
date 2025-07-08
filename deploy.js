5ba69185f63aafa1993ce2ce76b321dfdcb6590b
Ophelie Bugnon <ophelie.bugnon@edu.hefr.ch>
2025-07-08 15:11:18 +0200

    [fix] Fix pipeline

--- deploy.js
+++ deploy.js
@@ -1,5 +1,6 @@
 // deploy.js
 require("dotenv").config();
+
 console.log('Host:', process.env.FTP_HOST);
 console.log('User:', process.env.FTP_USER);
 console.log('Password:', process.env.FTP_PASSWORD ? 'OK' : 'Manquant');
