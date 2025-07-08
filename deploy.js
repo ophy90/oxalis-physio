0ea5b0064e5e34bdb0ccb8f1fcb0a22705fef0f3
Ophelie Bugnon <ophelie.bugnon@edu.hefr.ch>
2025-07-08 10:43:38 +0200

    [fix] Fixed deploy

--- deploy.js
+++ deploy.js
@@ -1,22 +1,26 @@
+// deploy.js
 require("dotenv").config();
+console.log('Host:', process.env.FTP_HOST);
+console.log('User:', process.env.FTP_USER);
+console.log('Password:', process.env.FTP_PASSWORD ? 'OK' : 'Manquant');
 const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
 
 const path = require("path");
 
 const config = {
-    user: process.env.FTP_USER,
+    user:process.env.FTP_USER,
     password: process.env.FTP_PASSWORD,
     host: process.env.FTP_HOST,
     port: 21,
     localRoot: path.join(__dirname, "build"),
     remoteRoot: process.env.FTP_REMOTE_PATH,
     include: ["*", "**/*"],
-    deleteRemote: false, // true if replace all at each deploy
+    deleteRemote: true, // replace the previous files
     forcePasv: true,
 };
 
-console.log("📦 Build ready. Starting FTP deploy...");
+console.log("Build ready. Starting FTP deploy...");
 
 ftpDeploy
     .deploy(config)
